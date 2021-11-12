83735529d0a05ec319eac1368a442d7cd19c626c
yushoyamaguchi <ysh.824@outlook.jp>
2021-11-12 13:13:23 +0900

    docker(iperf)+ping結果取得

--- house/exp5/ptest1.cpp
+++ house/exp5/ptest1.cpp
@@ -8,20 +8,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char ping[]="ping -c 1 192.168.12.1";
+const char ping[]="ping -c 3 192.168.12.1";
 FILE* fp;
 char  buf[1024];
 
-
+double rtt(char *){
+    return 0;
+}
 
 int main(){
+    int count;
     
     for(int i=0;i<3;i++){
-        
-        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+        count=0;
+        std::this_thread::sleep_for(std::chrono::milliseconds(30));
         if ((fp = popen(ping, "r")) != NULL) {
             while (fgets(buf, sizeof(buf), fp) != NULL) {
-                printf("%s", buf);
+                count++;
+                if(count==8)printf("%s", buf);
             }
             pclose(fp);
         }
