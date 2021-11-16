59551ea84ae2c1db02d1d159968aa30bda02293b
yushoyamaguchi <ysh.824@outlook.jp>
2021-11-16 22:09:48 +0900

    ping秒数取得

--- house/exp6/ptest1.cpp
+++ house/exp6/ptest1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <thread>
 #include <cstdio>
 #include <cstdint>
@@ -7,21 +8,45 @@
 #include <string>    // string
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <istream>
+#include <vector>
 
-const char ping[]="ping -c 2 2001:2::2 -I 2001:12::101";
+#define PING_LINES 5
+#define PING_NUM 2
+#define RTT_AVE 4
+
+
 FILE* fp;
 char  buf[1024];
 
-double rtt(char *){
-    return 0;
+double get_rtt(char * rtt_rtn){
+    std::vector<std::string> v;
+    std::string rtt_str=rtt_rtn;
+    std::stringstream ss{rtt_str};
+    std::string buf;
+    while (std::getline(ss, buf, '/')) {
+        v.push_back(buf);
+    }
+
+    double rtt=stod(v[RTT_AVE]);
+    return rtt;
 }
 
 void show_ping(){
     int count=0;
+    double rtt_val;
+    std::string ping_str="ping -c ";
+    std::string num = std::to_string(PING_NUM);
+    ping_str.append(num);
+    ping_str.append(" 2001:2::2 -I 2001:12::101");
+    char const *ping = ping_str.c_str();
     if ((fp = popen(ping, "r")) != NULL) {
         while (fgets(buf, sizeof(buf), fp) != NULL) {
             count++;
-            if(count==7){
+            if(count==PING_LINES+PING_NUM){
+                rtt_val=get_rtt(buf);
+                printf("%lf\n",rtt_val);
                 printf("%s", buf);
             }
         }
