cd22ec315189bcf4d00fdf45596f4c958dd6b439
yushoyamaguchi <ysh.824@outlook.jp>
2021-11-16 23:27:42 +0900

    時間計測&経路変更テスト1成功

--- house/exp6/ptest1.cpp
+++ house/exp6/ptest1.cpp
@@ -15,6 +15,7 @@
 #define PING_LINES 5
 #define PING_NUM 2
 #define RTT_AVE 4
+#define BIG 1024
 
 
 FILE* fp;
@@ -33,7 +34,7 @@ double get_rtt(char * rtt_rtn){
     return rtt;
 }
 
-void show_ping(){
+double ping_rtt_return(){
     int count=0;
     double rtt_val;
     std::string ping_str="ping -c ";
@@ -46,25 +47,46 @@ void show_ping(){
             count++;
             if(count==PING_LINES+PING_NUM){
                 rtt_val=get_rtt(buf);
-                printf("%lf\n",rtt_val);
+                
                 printf("%s", buf);
             }
         }
         pclose(fp);
+        return rtt_val;
+    }
+    else {
+        return BIG;
+    }
+}
+
+void time_check_route_decide(double route1, double route2){
+    if(route1<route2){
+            system("ip -6 route del 2001:2::0/64 table 100");
+            system("ip -6 route add 2001:2::0/64 encap seg6 mode encap segs 2::20,4::10 dev net0 table 100");
+            printf("route1\n");
+        }
+        else{
+            system("ip -6 route del 2001:2::0/64 table 100");
+            system("ip -6 route add 2001:2::0/64 encap seg6 mode encap segs 3::20,4::10 dev net0 table 100");
+            printf("route2\n");
     }
 }
 
 int main(){
     int count=0;
+    double route1,route2=0;
     for(count=0;count<3;count++){
-        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        
         system("ip -6 rule del from 2001:12::101");
         system("ip -6 rule add from 2001:12::101 table 130");
-        show_ping();
-        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        route1=ping_rtt_return();
+        
         system("ip -6 rule del from 2001:12::101");
         system("ip -6 rule add from 2001:12::101 table 140");
-        show_ping();
+        route2=ping_rtt_return();
+        time_check_route_decide(route1,route2);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
     }
     
     return EXIT_SUCCESS;
